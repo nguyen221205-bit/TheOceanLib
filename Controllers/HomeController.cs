f017c7e9cc4a4a707372c5a896b456d1b8d99d5f
PhamMinhTai <nguyen221205@gmail.com>
2026-07-25 12:11:55 +0700

    Initial commit: Setup premium Navy & Emerald library layout and homepage

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_LTWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+        
+    }
+}
